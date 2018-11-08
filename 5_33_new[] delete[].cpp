1dfa1a01b4d815671555dbec9f98c278a77ae8c6
zblan <zblan@gmail.com>
2018-11-08 14:20:24 +0800

    practise

---
+++ 5_33_new[] delete[].cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdlib>
+class A{
+public:
+	A(int i = 0):pi(new int(i)){
+		std::cout << "A¹¹Ôì\n";
+	}
+	~A(){
+		std::cout << "AÎö¹¹\n";
+		delete pi;
+	} 
+	/*static*/void* operator new[](std::size_t size){
+		if(void * p = malloc(size)){
+			std::cout << "-A operator new[]\n";
+			return p;
+		}
+		throw bad_alloc();
+	}
+	/*static*/ void operator delete[](void* p){
+		std::cout << "A operator delete[]\n";
+		free(p);
+	}
+private:
+	int* pi;
+};
